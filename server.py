9f37e97672ec5eab2a2706e9c7d1606be858c9be
Max Maximov <maxss.k2n@yandex.ru>
2023-03-13 16:38:46 +0300

    #2 update

--- server.py
+++ server.py
@@ -125,6 +125,7 @@ def play_game(res, req):
             city = random.choice(list(cities))
         # записываем город в информацию о пользователе
         sessionStorage[user_id]['city'] = city
+        sessionStorage[user_id]['countr'] = False
         sessionStorage[user_id]['country'] = geo.get_geo_info(city, 'country')
         # добавляем в ответ картинку
         res['response']['card'] = {}
